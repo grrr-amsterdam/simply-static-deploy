d2336585c48ca3741d63137759b1937a584ab717
Koen Schaft <koen@grrr.nl>
2019-12-05 14:57:31 +0100

    Refactor Archive syntax to new standards

--- includes/Grrr/SimplyStaticDeploy/Archive.php
+++ includes/Grrr/SimplyStaticDeploy/Archive.php
@@ -5,21 +5,19 @@ use Simply_Static;
 use Garp\Functional as f;
 
 /**
- * @TODO Refactor syntax.
  * @TODO Make everything configurable via hooks and actions.
  */
 class Archive {
 
-    protected $_tasks = [];
-
-    protected $_additional_files_option = '';
+    private $tasks = [];
+    private $additionalFilesOption = '';
 
     public function __construct() {
         // Increase max execution time for this class.
         ini_set('max_execution_time', 600);
 
         // Add all tasks in the right order.
-        $this->_tasks = [
+        $this->tasks = [
             new Simply_Static\Setup_Task(),
             new Simply_Static\Fetch_Urls_Task(),
             new Simply_Static\Transfer_Files_Locally_Task(),
@@ -34,24 +32,24 @@ class Archive {
      */
     public function start() {
         // Run all preparation and configuration tasks.
-        $this->_set_start_options();
-        $this->_clear_directory(static::get_directory());
-        $this->_resolve_additional_files();
-        $this->_enforce_additional_files([
+        $this->set_start_options();
+        $this->clear_directory(static::get_directory());
+        $this->resolve_additional_files();
+        $this->enforce_additional_files([
             rtrim(get_template_directory(), '/') . '/assets/build',
         ]);
-        $this->_add_hidden_posts();
+        $this->add_hidden_posts();
 
         // Run all static site generation tasks.
-        foreach ($this->_tasks as $task) {
-            $result = $this->_perform_task($task);
+        foreach ($this->tasks as $task) {
+            $result = $this->perform_task($task);
             if ($result instanceof WP_Error) {
-                $this->_set_end_options();
+                $this->set_end_options();
                 do_action('grrr_simply_static_deploy_error', $result);
                 return $result;
             }
             if (!$result) {
-                $this->_set_end_options();
+                $this->set_end_options();
                 $message = 'Something went wrong in Simply Static: ' . get_class($task);
                 $error = new WP_Error('simply_static_error', $message, [
                     'status' => 403,
@@ -62,8 +60,8 @@ class Archive {
         }
 
         // Run all file modification and end tasks.
-        $this->_modify_generated_files();
-        $this->_set_end_options();
+        $this->modify_generated_files();
+        $this->set_end_options();
 
         return true;
     }
@@ -93,12 +91,12 @@ class Archive {
      *
      * @return Task|bool
      */
-    protected function _perform_task($task) {
+    private function perform_task($task) {
         $result = $task->perform();
-        return $result ?: $this->_perform_task($task);
+        return $result ?: $this->perform_task($task);
     }
 
-    protected function _set_start_options(): void {
+    private function set_start_options(): void {
         Simply_Static\Options::instance()
             ->set('archive_status_messages', [])
             ->set('archive_start_time', Simply_Static\Util::formatted_datetime())
@@ -106,9 +104,9 @@ class Archive {
             ->save();
     }
 
-    protected function _set_end_options(): void {
+    private function set_end_options(): void {
         Simply_Static\Options::instance()
-            ->set('additional_files', $this->_additional_files_option)
+            ->set('additional_files', $this->additionalFilesOption)
             ->set('archive_end_time', Simply_Static\Util::formatted_datetime())
             ->save();
     }
@@ -117,7 +115,7 @@ class Archive {
      * Clear the current static site directory, to make sure deleted pages
      * are not deployed again, and potentialy overwriting redirects.
      */
-    protected function _clear_directory(string $dir): void {
+    private function clear_directory(string $dir): void {
         $name = f\last(f\split('/', trim($dir, '/')));
         if (!$dir || !file_exists($dir) || $name !== 'static') {
             return;
@@ -148,9 +146,9 @@ class Archive {
      * We're fixing this by temporarily resolving all `additional_files` paths
      * to absolute URLs with resolved symlinks, and restoring them after completion.
      */
-    protected function _resolve_additional_files(): void {
+    private function resolve_additional_files(): void {
         $option = Simply_Static\Options::instance()->get('additional_files');
-        $this->_additional_files_option = $option;
+        $this->additionalFilesOption = $option;
 
         $paths = Simply_Static\Util::string_to_array($option);
 
@@ -165,7 +163,7 @@ class Archive {
      * built if anything goes wrong with restoring the original option (eg. saving
      * the resolved paths so they can't be resolved in the next release).
      */
-    protected function _enforce_additional_files(array $paths): void {
+    private function enforce_additional_files(array $paths): void {
         $option = Simply_Static\Options::instance()->get('additional_files');
         $paths = f\concat(
             Simply_Static\Util::string_to_array($option),
@@ -181,7 +179,7 @@ class Archive {
     /**
      * Fix some generated files since we'll be hosting them statically.
      */
-    protected function _modify_generated_files(): void {
+    private function modify_generated_files(): void {
         $cwd = rtrim(static::get_directory(), '/');
 
         // The `sitemap.xml` generated by Yoast redirects to `sitemap_index.xml`.
@@ -206,20 +204,20 @@ class Archive {
      * We'll have to fetch them manually and append them to the `additional_urls`
      * option ourselves.
      */
-    protected function _add_hidden_posts(): void {
+    private function add_hidden_posts(): void {
         $option = Simply_Static\Options::instance()->get('additional_urls');
         $existing_urls = Simply_Static\Util::string_to_array($option);
         Simply_Static\Options::instance()->set(
             'additional_urls',
             f\join(PHP_EOL, f\unique(f\concat(
                 $existing_urls,
-                $this->_fetch_password_protected_posts(),
-                $this->_fetch_yoast_noindex_posts()
+                $this->fetch_password_protected_posts(),
+                $this->fetch_yoast_noindex_posts()
             )))
         );
     }
 
-    protected function _fetch_password_protected_posts(): array {
+    private function fetch_password_protected_posts(): array {
         $query = new \WP_Query([
             'post_type' => 'any',
             'has_password' => true,
@@ -228,7 +226,7 @@ class Archive {
         return f\map(f\compose('get_permalink', f\prop('ID')), $query->posts);
     }
 
-    protected function _fetch_yoast_noindex_posts(): array {
+    private function fetch_yoast_noindex_posts(): array {
         $query = new \WP_Query([
             'post_type' => 'any',
             'meta_key' => '_yoast_wpseo_meta-robots-noindex',
