fc3d5f4a21bd5e477cf58803e9e0041b58d57dca
Harmen Janssen <harmen@grrr.nl>
2019-12-11 14:43:56 +0100

    Create nested config objects on the fly

--- includes/Grrr/SimplyStaticDeploy/Config.php
+++ includes/Grrr/SimplyStaticDeploy/Config.php
@@ -4,43 +4,43 @@ use Garp\Functional as f;
 
 class Config {
 
-    const REQUIRED_FIELDS = [
-        'aws' => [
-            'key',
-            'secret',
-            'region',
-            'bucket',
-        ],
-        'url',
-    ];
-
-    const OPTIONAL_FIELDS = [
-        'aws' => [
-            'bucket_acl',
-            'distribution',
-        ],
-    ];
-
     private $data;
+    private $requiredFields;
 
-    public function __construct(array $data) {
-        $this->validate($data);
+    public function __construct(array $data, array $requiredFields) {
+        $this->validate($data, $requiredFields);
         $this->data = $data;
+        $this->requiredFields = $requiredFields;
     }
 
-    public function __get(string $key): string {
-        return $this->data[$key] ?? '';
+    public function __get(string $key) {
+        $out = $this->data[$key] ?? '';
+        if (is_array($out)) {
+            return new self($out, $this->requiredFields[$key] ?? []);
+        }
+        return $out;
     }
 
-    private function validate(array $data): void {
+    private function validate(array $data, array $requiredFields, string $prefix = ''): void {
+        $requiredNestedKeys = array_keys(f\filter('is_array', $requiredFields));
+        $requiredKeys = f\concat(
+            f\filter('is_string', $requiredFields),
+            $requiredNestedKeys
+        );
+
         $missingProps = f\reject(
             f\prop_of($data),
-            static::REQUIRED_FIELDS
+            $requiredKeys
         );
         if (count($missingProps)) {
+            $namespacedProps = f\map(f\concat($prefix), $missingProps);
             throw new Exception(
-                sprintf('Missing required option(s): %s', implode(', ', $missingProps))
+                sprintf('Missing required option(s): %s', implode(', ', $namespacedProps))
             );
+        }
+
+        foreach ($requiredNestedKeys as $nestedKey) {
+            $this->validate($data[$nestedKey] ?? [], $requiredFields[$nestedKey], "$nestedKey.");
         }
     }
 
