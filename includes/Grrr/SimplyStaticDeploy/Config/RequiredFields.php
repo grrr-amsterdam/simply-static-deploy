15fc62dc3402de847aa52a277460c271da58fdb7
Harmen Janssen <harmen@grrr.nl>
2019-12-11 15:06:23 +0100

    Validate config structure and report to admins

--- includes/Grrr/SimplyStaticDeploy/Config/RequiredFields.php
+++ includes/Grrr/SimplyStaticDeploy/Config/RequiredFields.php
@@ -1,5 +1,7 @@
 <?php namespace Grrr\SimplyStaticDeploy\Config;
 
+use Garp\Functional as f;
+
 final class RequiredFields {
 
     const REQUIRED_FIELDS = [
@@ -11,6 +13,21 @@ final class RequiredFields {
         ],
         'url',
     ];
+
+    public static function getMissingOptions(array $data, array $requiredFields = self::REQUIRED_FIELDS, string $prefix = ''): array {
+        return f\reduce_assoc(
+            function (array $missing, $value, $key) use ($data, $prefix): array {
+                if (is_array($value)) {
+                    return f\concat($missing, static::getMissingOptions($data[$key] ?? [], $value, "{$key}."));
+                }
+                return !isset($data[$value])
+                    ? f\concat($missing, [$prefix . $value])
+                    : $missing;
+            },
+            [],
+            $requiredFields
+        );
+    }
 
     public static function toArray(): array {
         return static::REQUIRED_FIELDS;
