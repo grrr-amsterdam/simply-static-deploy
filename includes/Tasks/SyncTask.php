210c69c32deb4a9ae6fda96827bde02dcd588174
Ramiro Hammen <ramiro@grrr.nl>
2020-10-19 15:41:37 +0200

    Remove deprecated code

--- includes/Tasks/SyncTask.php
+++ includes/Tasks/SyncTask.php
@@ -2,9 +2,9 @@
 
 namespace Grrr\SimplyStaticDeploy\Tasks;
 
-use Grrr\SimplyStaticDeploy\Archiver;
 use Grrr\SimplyStaticDeploy\Config;
 use Grrr\SimplyStaticDeploy\SimplyStaticDeploy;
+use Grrr\SimplyStaticDeploy\StaticDeployJob;
 use Grrr\SimplyStaticDeploy\Syncer;
 use Simply_Static\Task;
 use Simply_Static\Util;
@@ -15,12 +15,12 @@ class SyncTask extends Task
 
     public function perform()
     {
-        Util::debug_log('Sync shizzle to s3');
+        Util::debug_log('Sync site to s3');
         $this->save_status_message('Syncing files to S3');
         $config = new Config(constant(SimplyStaticDeploy::CONFIG_CONST));
 
         $syncer = new Syncer($config->aws);
-        $path = Archiver::get_directory();
+        $path = StaticDeployJob::get_directory();
 
         return $syncer->sync($path);
     }
