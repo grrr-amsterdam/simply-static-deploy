5e43368b944fe9b88dd06f5234c8d5c05c91a332
Jochem de Groot <jochem@grrr.nl>
2022-07-05 16:31:47 +0200

    Update plugin version to v2.2.0

--- simply-static-deploy.php
+++ simply-static-deploy.php
@@ -3,14 +3,14 @@
  * Plugin Name: Simply Static Deploy
  * Plugin URI:  https://github.com/grrr-amsterdam/simply-static-deploy/
  * Description: Deploy static sites easily to an AWS S3 bucket.
- * Version:     2.1.3
+ * Version:     2.2.0
  * Author:      GRRR
  * Author URI:  https://grrr.nl
  */
 use Grrr\SimplyStaticDeploy\SimplyStaticDeploy;
 
 // Global constants.
-define('SIMPLY_STATIC_DEPLOY_VERSION', '2.1.3');
+define('SIMPLY_STATIC_DEPLOY_VERSION', '2.2.0');
 define('SIMPLY_STATIC_DEPLOY_PATH', plugin_dir_path(__FILE__));
 define('SIMPLY_STATIC_DEPLOY_URL', plugin_dir_url(__FILE__));
 
